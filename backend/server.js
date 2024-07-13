0a3c18b1c33c9c3466a7b03626e0e8839b0d3443
Luiz Eduardo <lvieiradepaulo00@gmail.com>
2024-07-13 14:49:10 -0700

    backend set up

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+import apiRouter from "./routes/api.js"
+
+// Constants
+const port = process.env.PORT || 3000;
+
+// Create http server
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+app.use(express.static(path.join("../client/")));
+
+app.use("/api", apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+// Start http server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
