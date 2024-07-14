ba76c6d9e3a10565c9c26071d597969dc4cccdd5
Grace Barros <gracepbarros@gmail.com>
2024-07-13 23:18:07 -0700

    bugfix/className-read-me

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
       <h2>Explore more titles</h2>
       <div className="search">
         <input type="text" placeholder="Search for a book..." />
-        <span class="material-symbols-outlined">search</span>
+        <span className="material-symbols-outlined">search</span>
       </div>
     </main>
   );
