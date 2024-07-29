91226f5242fdc1dbbfafd3b8cbbf0badfd585dbb
gracepbarros <gracepbarros@gmail.com>
2024-07-28 21:08:19 -0700

    add api endpoints

--- backend/routes/bookRouter.js
+++ backend/routes/bookRouter.js
@@ -1,36 +1,74 @@
 import express from "express";
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const router = express.Router()
 const API_URL = 'https://www.googleapis.com/books/v1/volumes';
+const API_KEY = process.env.GOOGLE_API_KEY;
 
-router.get("/:search", async (req, res) => {
-    const query = req.params.search;
+function formatBookData(rawitem){
+    const { id, volumeInfo } = rawitem;
+        
+    const { 
+        title, 
+        authors, 
+        description,
+        pageCount, 
+        imageLinks, } = volumeInfo;
 
-    if (!query) {
+    return {
+        googleId: id,
+        title,
+        authors,
+        description,
+        pageCount,
+        cover: imageLinks.thumbnail, };
+}
+
+// Book search - get a list of books
+router.get("/", async (req, res) => {
+    try {
+      const query = req.query.q;
+
+      if (!query) {
         res.status(400).send('Missing query parameter');
-    }
-    console.log(query)
-
-    //const limit = req.query.limit || 10;
-      try {
-       //const response = await axios.get(`https://openlibrary.org/search.json?q=${query}&limit=${limit}`);
-      const response = await axios.get(`https://openlibrary.org/search.json?q=${query}`);
-      const data = JSON.parse(response.data);
-  
-      if (data.numFound === 0) {
+      }
+    
+      const limit = req.query.limit || 10;
+      const response = await axios.get(`${API_URL}?q=${query}&limit=${limit}&key=${API_KEY}`);
+      console.log("Test of response: ",response.data.items)
+      const { items, totalItems } = response.data;
+        
+      if (totalItems == 0) {
         return res.status(404).send('No books found');
       }
-      console.log('Book Controller:');
-      console.log(data);
-      res.json(data);
+
+      const formattedItems = items.map((item) => {
+        return {
+            ...formatBookData(item),
+        };
+      });
+
+      res.json(formattedItems);
         
     } catch (error) {
-      console.error('Error on API searching:', error);
+      console.error('Failed to Fetch books.');
       res.status(500).send('Error on API searching');
     }
 
 });
 
+// Book details - get a book by ID
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+      const response = await axios.get(`${API_URL}/${id}?key=${API_KEY}`);
+      res.json(formatBookData(response.data));
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch book details', message: error.message });
+    }
+  });
 
 export default router;
