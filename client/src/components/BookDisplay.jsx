ddaf00838996f5fc276fcb58e7c232019b7c4f26
Piero Friedrich <pierofriedrich@yahoo.com.br>
2024-08-01 22:19:36 -0700

    match bookcard with api

--- client/src/components/BookDisplay.jsx
+++ client/src/components/BookDisplay.jsx
@@ -1,31 +1,40 @@
-import React, { useState} from 'react';
-import BookCard from './BookCard';
-import Modal from './Modal';
+import React, { useState } from "react";
+import BookCard from "./BookCard";
+import Modal from "./Modal";
 
-const BookDisplay = ({ book }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+const BookDisplay = ({ book, userId }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const openCard = () => {
-        setIsModalOpen(true);
-    };
+  const openCard = () => {
+    setIsModalOpen(true);
+  };
 
-    const closeCard = () => {
-        setIsModalOpen(false);
-    };
+  const closeCard = () => {
+    setIsModalOpen(false);
+  };
 
-    return (
-        <>
-            <div className="book-card bgbrown mx-1 flex-col items-center content-between" onClick={openCard}>
-                <img src={book.cover} alt={`${book.title} cover`}  className='w-[100%]'/>
-                <p className='irishgrover text-center self-end textbeige text-3x1 p-2 h-[80%]'>{book.title}</p>
-            </div>
-            {isModalOpen && (
-                <Modal onClose={closeCard}>
-                    <BookCard book={book} />
-                </Modal>
-            )}
-        </>
-    );
+  return (
+    <>
+      <div
+        className="book-card bgbrown mx-1 flex-col items-center content-between"
+        onClick={openCard}
+      >
+        <img
+          src={book.cover}
+          alt={`${book.title} cover`}
+          className="w-[100%]"
+        />
+        <p className="irishgrover text-center self-end textbeige text-3x1 p-2 h-[80%]">
+          {book.title}
+        </p>
+      </div>
+      {isModalOpen && (
+        <Modal onClose={closeCard}>
+          <BookCard googleId={book.googleId} userId={userId} />
+        </Modal>
+      )}
+    </>
+  );
 };
 
 export default BookDisplay;
